92080ca2c20c8a5cd6675f9977b794f728c76ac1
Ramesh Rasaiyan <rasaiyan.ramesh@gmail.com>
2015-11-21 23:22:31 -0600

    cleaned up the code, moved few items from controller to servies

--- controllers.js
+++ controllers.js
@@ -1,32 +1,22 @@
 // Controllers
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('homeController', ['$scope', 'cityService', '$location', function($scope, cityService, $location){
   $scope.city = cityService.city;
 
   $scope.$watch('city', function() {
     cityService.city = $scope.city;
   });
+
+  $scope.submit = function() {
+    $location.path("/forecast");
+  }
+
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService){
   $scope.city = cityService.city;
   $scope.days = $routeParams.days || '2';
 
-  $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
-    {
-      callback: 'JSON_CALLBACK'
-    },
-    {
-      get: {
-        method: 'JSONP'
-      }
-    }
-  );
-
-  $scope.weatherResult = $scope.weatherAPI.get({
-    q: $scope.city,
-    cnt: $scope.days,
-    APPID: "90f7f9b786b069825bcee1c062e9109c"
-  });
+  $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days, $scope.appid);
 
   $scope.convertToFahrenheit = function(degK) {
     return Math.round((1.8 * (degK - 273)) + 32);
--- services.js
+++ services.js
@@ -2,3 +2,25 @@
 weatherApp.service('cityService', function() {
   this.city = "New York, NY";
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+
+  this.GetWeather = function(city, days, appid) {
+    var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
+      {
+        callback: 'JSON_CALLBACK'
+      },
+      {
+        get: {
+          method: 'JSONP'
+        }
+      }
+    );
+
+    return weatherAPI.get({
+      q: city,
+      cnt: days,
+      APPID: "90f7f9b786b069825bcee1c062e9109c"
+    });
+  };
+}]);
