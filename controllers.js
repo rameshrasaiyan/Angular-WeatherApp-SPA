3f2a554b4f76491c92103f7e18bacfe5cc86ebd8
Ramesh Rasaiyan <rasaiyan.ramesh@gmail.com>
2015-11-21 22:40:12 -0600

    Initial Commit

---
+++ controllers.js
@@ -0,0 +1,39 @@
+// Controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
+  $scope.city = cityService.city;
+  $scope.days = $routeParams.days || '2';
+
+  $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
+    {
+      callback: 'JSON_CALLBACK'
+    },
+    {
+      get: {
+        method: 'JSONP'
+      }
+    }
+  );
+
+  $scope.weatherResult = $scope.weatherAPI.get({
+    q: $scope.city,
+    cnt: $scope.days,
+    APPID: "90f7f9b786b069825bcee1c062e9109c"
+  });
+
+  $scope.convertToFahrenheit = function(degK) {
+    return Math.round((1.8 * (degK - 273)) + 32);
+  }
+
+  $scope.convertToDate = function(dt) {
+    return new Date(dt * 1000);
+  }
+
+}]);
